23b64689c649952133c0a1e6152de0bc1bcd21a4
diana2456 <dk5500103@gmail.com>
2022-10-16 01:57:47 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,40 @@
+import java.util.ArrayList;
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<String> A = new ArrayList<>();
+        A.add("ик");
+        A.add("при");
+        A.add("очки");
+        A.add("цветы");
+        A.add("привет");
+        System.out.println(A);
+
+        ArrayList<String> B = new ArrayList<>();
+        B.add("1");
+        B.add("2");
+        B.add("3");
+        B.add("4");
+        B.add("5");
+
+        ArrayList<String> C = new ArrayList<>();
+        int i = 0;
+        int i1 = 1;
+        int i2 = 2;
+        int i3 = 3;
+        int i4 = 4;
+        C.add(A.get(i) + B.get(i + 0));
+        C.add(A.get(i1) + B.get(i1 + 0));
+        C.add(A.get(i2) + B.get(i2 + 0));
+        C.add(A.get(i3) + B.get(i3 + 0));
+        C.add(A.get(i4) + B.get(i4 + 0));
+        System.out.println(C);
+        for (String ret:C) {
+            System.out.println(ret);
+        }
+    }
+}
+
+
+
+
+
